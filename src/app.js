22ac50b8586dba86e0273feb5112ed35de77b923
KamajiiSC <brandonlschaen@gmail.com>
2020-05-09 00:03:14 -0400

    Set activeClassName for the NavLinks

--- src/app.js
+++ src/app.js
@@ -37,10 +37,10 @@ const NotFound = () => (
 const Header = () => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to='/'>Dashboard</NavLink>
-    <NavLink to='/create'>Create Expense</NavLink>
-    <NavLink to='/edit'>Edit Expense</NavLink>
-    <NavLink to='/help'>Help</NavLink>
+    <NavLink to='/' activeClassName='is-active'>Dashboard</NavLink>
+    <NavLink to='/create' activeClassName='is-active'>Create Expense</NavLink>
+    <NavLink to='/edit' activeClassName='is-active'>Edit Expense</NavLink>
+    <NavLink to='/help' activeClassName='is-active'>Help</NavLink>
   </header>
 );
 
