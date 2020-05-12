9e021d4d91b7f5c9618264723de660a568718bc8
KamajiiSC <brandonlschaen@gmail.com>
2020-05-11 22:06:39 -0400

    Changed the if else in store over to switch

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,12 +1,13 @@
 import { createStore } from 'redux';
 
 const store = createStore((state = { count: 0 }, action) => {
-  if (action.type === 'INCREMENT') {
-    return {
-      count: state.count + 1
-    };
-  } else {
-    return state;
+  switch(action.type) {
+    case 'INCREMENT':
+      return{
+        count: state.count + 1
+      };
+      default:
+        return state;
   }
 });
 
