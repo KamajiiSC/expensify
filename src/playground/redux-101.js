635c9151671cf54e021c0446899679885e547b02
KamajiiSC <brandonlschaen@gmail.com>
2020-05-11 22:51:25 -0400

    Moved decrementBy to DECREMENT

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -4,11 +4,11 @@ const store = createStore((state = { count: 0 }, action) => {
   switch(action.type) {
     case 'INCREMENT':
       const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return{
         count: state.count + incrementBy
       };
     case 'DECREMENT': 
+    const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return{
         count: state.count - decrementBy
       };
