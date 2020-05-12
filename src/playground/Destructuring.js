41ec6ee84af2b21a28a028a861f36d0eaaae5076
KamajiiSC <brandonlschaen@gmail.com>
2020-05-12 17:06:49 -0400

    Used object destructuring to make the code a lot cleaner

--- src/playground/Destructuring.js
+++ src/playground/Destructuring.js
@@ -10,6 +10,7 @@ const person = {
 const { name, age } = person;
 console.log(`${name} is ${age}.`);
 
-if(person.location.city && person.location.temp) {
-  console.log(`It's ${person.location.temp} in ${person.location.city}.`);
+const { city, temp } = person.location;
+if(city && temp) {
+  console.log(`It's ${temp} in ${city}.`);
 }
