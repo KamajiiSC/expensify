24f4a77b0ae2fa67c315dc745a671c3475f83a88
KamajiiSC <brandonlschaen@gmail.com>
2020-05-11 22:04:00 -0400

    Inside of an if else statement return state, or if action is INCREMENT then change the count

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,13 @@
 import { createStore } from 'redux';
 
 const store = createStore((state = { count: 0 }, action) => {
-  return state;
+  if (action.type === 'INCREMENT') {
+    return {
+      count: state.count + 1
+    };
+  } else {
+    return state;
+  }
 });
 
 console.log(store.getState());
