5b33233608b44d2cc65ba52b1ce17eda8bdb4a84
KamajiiSC <brandonlschaen@gmail.com>
2020-05-12 17:17:37 -0400

    Commented out code to create new object

--- src/playground/Destructuring.js
+++ src/playground/Destructuring.js
@@ -1,16 +1,16 @@
-const person = {
-  name: 'Brandon',
-  age:22,
-  location: {
-    city: 'Baltimore',
-    temp:60
-  }
-};
+// const person = {
+//   name: 'Brandon',
+//   age:22,
+//   location: {
+//     city: 'Baltimore',
+//     temp:60
+//   }
+// };
 
-const { name: firstName = 'Anonymous', age } = person;
-console.log(`${firstName} is ${age}.`);
+// const { name: firstName = 'Anonymous', age } = person;
+// console.log(`${firstName} is ${age}.`);
 
-const { city, temp: temperature } = person.location;
-if(city && temperature) {
-  console.log(`It's ${temperature} in ${city}.`);
-}
+// const { city, temp: temperature } = person.location;
+// if(city && temperature) {
+//   console.log(`It's ${temperature} in ${city}.`);
+// }
