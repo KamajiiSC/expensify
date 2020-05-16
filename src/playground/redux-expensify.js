02997a2ffa28eb73ebabc7f502da62cc1d17c807
KamajiiSC <brandonlschaen@gmail.com>
2020-05-15 23:48:52 -0400

    defined id description note amount and createdAt in expenses

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -2,6 +2,10 @@ import { createStore, combineReducers } from 'redux';
 
 const demoState = {
   expense: [{
-    
+    id: 'poihasfj',
+    description: 'January Rent',
+    note: 'This was final payment for address',
+    amount: 54500,
+    createdAt: 0
   }]
 };
