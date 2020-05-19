89af9746648c8af519d6b23c8e5a78572b1bf3ea
KamajiiSC <brandonlschaen@gmail.com>
2020-05-18 21:49:21 -0400

    Created case EDIT_EXPENSE. It maps through state and checks if expense.id === the action id otherwise it returns the existing expense

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -49,8 +49,16 @@ const expensesReducer = (state = expensesReducerDefaultState, action) =>  {
         ...state,
         action.expense
       ];
-      case 'REMOVE_EXPENSE':
-        return state.filter(({id})=> id !== action.id);
+    case 'REMOVE_EXPENSE':
+      return state.filter(({id})=> id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) =>{
+        if(expense.id === action.id) {
+
+        } else {
+          return expense;
+        }
+      });
     default:
       return state;
   }
