389174b2da79ac94516c49269cefacd0462b0b7a
KamajiiSC <brandonlschaen@gmail.com>
2020-05-21 15:05:32 -0400

    Set sortBy to date in SORT_BY_DATE

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -95,7 +95,7 @@ const filterReducer = (state = filterReducerDefaultState, action) => {
     case 'SORT_BY_DATE':
       return{
         ...state,
-        
+        sortBy: 'date'
       }
     default:
       return state;
