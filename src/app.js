a1f4414e9388e815ea3fb91ac26197138c4c9ee2
KamajiiSC <brandonlschaen@gmail.com>
2020-05-22 15:44:01 -0400

    Imported setTextFilter and called it

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -12,5 +14,7 @@ store.dispatch(addExpense({description: 'Water bill', amount: 250, createdAt: 5}
 store.dispatch(addExpense({description: 'Gas bill', amount: 100, createdAt: 10}));
 
 console.log(store.getState());
+
+setTextFilter();
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
