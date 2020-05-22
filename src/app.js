19914ce7c8d187c9758654e7ea9777309a1a35f3
KamajiiSC <brandonlschaen@gmail.com>
2020-05-22 15:46:35 -0400

    Import getVisibleExpenses from expenses.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
+import { getVisibleExpenses } from './selectors/expenses';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -16,5 +17,8 @@ store.dispatch(addExpense({description: 'Gas bill', amount: 100, createdAt: 10})
 console.log(store.getState());
 
 setTextFilter('water');
+
+
+
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
