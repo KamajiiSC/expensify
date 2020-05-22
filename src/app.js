c355403b3d6cd0223ca5ccfdd55ae9bef393560c
KamajiiSC <brandonlschaen@gmail.com>
2020-05-22 15:41:18 -0400

    Created another dispatch for gas bill

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import './styles/styles.scss';
 const store = configureStore();
 
 store.dispatch(addExpense({description: 'Water bill', amount: 250, createdAt: 5}));
+store.dispatch(addExpense({description: 'Gas bill', amount: 100, createdAt: 10}));
 
 console.log(store.getState());
 
