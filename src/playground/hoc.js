74b453c7e89b0d722f470ad6be06d7d89ef7b356
KamajiiSC <brandonlschaen@gmail.com>
2020-05-24 17:02:08 -0400

    Passed props into info

--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -3,9 +3,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const info = () => {
+const info = (props) => {
   <div>
     <h1>info</h1>
-    <p>The info is: </p>
+    <p>The info is: {props.info}</p>
   </div>
 }
+
