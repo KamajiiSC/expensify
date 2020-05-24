89c106ae6550a082b200deff35ba7c181afee182
KamajiiSC <brandonlschaen@gmail.com>
2020-05-24 17:01:27 -0400

    Imported react and reactdom and made component info with h1 and p

--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -1,1 +1,11 @@
 // Higher Order Components - component that renders another component
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const info = () => {
+  <div>
+    <h1>info</h1>
+    <p>The info is: </p>
+  </div>
+}
