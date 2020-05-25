569ceeabc44f92a42d6f1b24565262914296d7e2
KamajiiSC <brandonlschaen@gmail.com>
2020-05-25 18:52:42 -0400

    Created p tag for created at

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -4,5 +4,6 @@ const ExpenseListItem = (props) => (
   <div>
     <h2>{props.expenses.description}</h2>
     <p>Amount: {props.expenses.amount}</p>
+    <p>Created at: {props.expenses.createdAt}</p>
   </div>
 );
