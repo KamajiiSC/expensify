e3c3e237cda895b1f44f547c08d47124b7818b9a
KamajiiSC <brandonlschaen@gmail.com>
2020-05-26 20:58:53 -0400

    Imported connect and set connect in the export

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const ExpenseListFilters = () => (
   <div>
@@ -6,4 +7,4 @@ const ExpenseListFilters = () => (
   </div>
 );
 
-export default ExpenseListFilters;
+export default connect()(ExpenseListFilters);
