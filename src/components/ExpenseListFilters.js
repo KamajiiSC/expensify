f6353cdb28bac1b62a9d8f9c9d834221bafd6cf5
KamajiiSC <brandonlschaen@gmail.com>
2020-05-26 21:13:06 -0400

    Imported setTextFilter and dispatched in onchange

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { setTextFilter } from '../actions/filters';
 
 const ExpenseListFilters = (props) => (
   <div>
-    <input type='text' value={props.filter.text} onChange={(e) =>
-      
+    <input type='text' value={props.filter.text} onChange={(e) => (
+      props.dispatch(setTextFilter(e.target.value))
     )}/>
   </div>
 );
