d24b14e6ee437a01fc9953832a8151baa6819e43
KamajiiSC <brandonlschaen@gmail.com>
2020-05-26 21:33:10 -0400

    Passed in dispatch and id, then dispatched a removeExpenses call

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
     <h2>{description}</h2>
     <p>Amount: {amount}</p>
     <p>Created at: {createdAt}</p>
     <button onClick={() => (
-      
+      dispatch(removeExpense({id}))
     )}>
     Remove</button>
   </div>
