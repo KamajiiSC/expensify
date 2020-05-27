f5aca0fbd153506b38896c016ec18b0a95a8219a
KamajiiSC <brandonlschaen@gmail.com>
2020-05-26 21:17:54 -0400

    Imported removeExpense from actions expenses

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
 const ExpenseListItem = ({ description, amount, createdAt }) => (
   <div>
