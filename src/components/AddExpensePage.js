107fb5168dc0856a9cbd09ce2ecb544ebf8da3b6
KamajiiSC <brandonlschaen@gmail.com>
2020-06-01 15:14:58 -0400

    Passed props to AddExpensePage

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses'
 
-const AddExpensePage = () => (
+const AddExpensePage = (props) => (
   <div>
     <h1>Add Expense</h1>
     <ExpenseForm 
       onSubmit={(expense) => {
-        console.log(expense);
+        props.dispatch(addExpense(expense));
       }}
     />
   </div>
