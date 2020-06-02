1801544fc08abfcfe9439286ca5bb6b8b60e1bf8
KamajiiSC <brandonlschaen@gmail.com>
2020-06-01 21:34:36 -0400

    Removed  redundant destructuring

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
+const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}>
      <h2>{description}</h2>
