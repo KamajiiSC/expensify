4f2a07b9cac91391d564e685efdcf73636546159
KamajiiSC <brandonlschaen@gmail.com>
2020-06-05 16:53:34 -0400

    Made ExpenseListFilters class based

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -2,6 +2,31 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { sortByDate, sortByAmount, setTextFilter } from '../actions/filters';
 
+class ExpenseListFilters extends React.Component { 
+  render() {
+    return (
+    <div>
+      <input type='text' value={props.filter.text} 
+      onChange={(e) => (
+        props.dispatch(setTextFilter(e.target.value))
+      )}/>
+      <select 
+      value={props.filter.sortBy}
+      onChange = {(e) => {
+        if(e.target.value === 'date'){
+          props.dispatch(sortByDate());
+        }else if(e.target.value === 'amount'){
+          props.dispatch(sortByAmount());
+        }
+      }}>
+        <option value='date'>Date</option>
+        <option value='amount'>Amount</option>
+      </select>
+    </div>
+    )
+  }
+}
+
 const ExpenseListFilters = (props) => (
   <div>
     <input type='text' value={props.filter.text} 
