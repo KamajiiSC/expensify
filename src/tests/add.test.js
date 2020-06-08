3728da4d9cd40b7637c0a2198dd071adb8848170
KamajiiSC <brandonlschaen@gmail.com>
2020-06-08 19:47:36 -0400

    Defined greeting as a call to generateGreeting

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -4,6 +4,7 @@ const generateGreeting = (name) => `Hello ${name}!`;
 
 test('should add two numbers', () => {
   const result = add(3, 4);
+  const greeting = generateGreeting('Mike');
 
   expect(result).toBe(7);
 });
