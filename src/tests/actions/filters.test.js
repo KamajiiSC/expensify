b6010fbcdf5bd7893724a2c69a2f67bef71a3472
KamajiiSC <brandonlschaen@gmail.com>
2020-06-09 15:01:07 -0400

    Created another test for generating end date action object

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -3,3 +3,7 @@ import { setStartDate, setEndDate } from '../../actions/filters';
 test('should generate set start date action object', () => {
 
 });
+
+test('should generate set end date action object', () => {
+
+});
