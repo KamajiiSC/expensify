4e3d834fc43827dba3dd101a1472df4d3a0202de
KamajiiSC <brandonlschaen@gmail.com>
2020-06-09 16:24:11 -0400

    Created test to generate SORT_BY_DATE

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import { setStartDate, setEndDate, setTextFilter } from '../../actions/filters';
+import { setStartDate, setEndDate, setTextFilter, sortByDate } from '../../actions/filters';
 
 test('should generate set start date action object', () => {
   const action = setStartDate(moment(0));
@@ -30,5 +30,12 @@ test('should generate set text filter action object with default value', () => {
   expect(action).toEqual({
     type: 'SET_TEXT_FILTER',
     text: ''
+  })
+});
+
+test('should generate sort by date action object', () => {
+  const action = sortByDate();
+  expect (action).toEqual({
+    type: 'SORT_BY_DATE'
   })
 });
