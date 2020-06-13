f4bb970924d17959d5f444b89784264a6623219c
KamajiiSC <brandonlschaen@gmail.com>
2020-06-13 17:03:43 -0400

    Set created at to a moment 4 days before or after to two of the examples

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -8,17 +8,18 @@ const expenses = [{
   amount: 195,
   createdAt: 0
 }, {
+
   id: '2',
   description: 'rent',
   note: '', 
   amount: 19500,
-  createdAt: -1000
+  createdAt: moment(0).subtract(4, 'days').valueOf()
 }, {
   id: '3',
   description: 'Credit Card',
   note: '', 
   amount: 4500,
-  createdAt: 1000
+  createdAt: moment(0).add(4, 'days'). valueOf()
 }]
 
 test('should filter by test value', ()=> {
