2a2e4d41395ac8164916211541a89255a78dbdbd
KamajiiSC <brandonlschaen@gmail.com>
2020-06-15 14:49:30 -0400

    Expect state to equal blank text and sortBy date

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -2,4 +2,9 @@ import filtersReducer from '../../reducers/filters';
 
 test ('should setup default filter values', () => {
   const state = filtersReducer(undefined,{type: '@@INIT' });
+  expect(state).toEqual({
+    text: '',
+    sortBy: 'date',
+    
+  })
 })
