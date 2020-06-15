f23e81bb0d0969002d7c6a9f8f98df2a2d2ee903
KamajiiSC <brandonlschaen@gmail.com>
2020-06-15 18:22:34 -0400

    Created test for endDate filter

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -37,6 +37,11 @@ test(' should set text filter', () => {
 // should set startDate filter
 test('should set startDate filter', () => {
   const state = filtersReducer( undefined, { type: 'SET_STARTDATE_FILTER', startDate: moment(0) });
+  expect(state.startDate).toBe
+  (moment(0));
 });
 
 //should set endDate filter
+test('should set endDate filter', () => {
+
+});
