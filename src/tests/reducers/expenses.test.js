9e567f1f1d4862550d9807a141e127459ba86f77
KamajiiSC <brandonlschaen@gmail.com>
2020-06-19 19:59:36 -0400

    Defined state and expected state to equal blank array

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -1,5 +1,6 @@
 import expensesReducer from '../../reducers/expenses';
 
 test('should set defaulkt state', () => {
-
+  const state = expensesReducer(undefined, { type: '@@INIT' });
+  expect(state).toEqual([]);
 });
