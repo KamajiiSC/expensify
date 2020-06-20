78864d43a7048c39351b29107835c22684dc9ad0
KamajiiSC <brandonlschaen@gmail.com>
2020-06-19 20:22:10 -0400

    Created new test for remove expense by id and created action object and targeted the second expense

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -5,3 +5,10 @@ test('should set defaulkt state', () => {
   const state = expensesReducer(undefined, { type: '@@INIT' });
   expect(state).toEqual([]);
 });
+
+test('should remove expense by id', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: expenses[1].id
+  }
+})
