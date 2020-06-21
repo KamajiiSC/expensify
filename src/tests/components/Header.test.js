b00b2d07a62d0a81896adbbd787617ad58b686da
KamajiiSC <brandonlschaen@gmail.com>
2020-06-20 22:36:40 -0400

    Imported React and Header

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,2 +1,4 @@
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import React from 'react';
+import ReactShallowRenderer from 'react-test-renderer/shallow';
+import Header from '../../components/Header';
 
