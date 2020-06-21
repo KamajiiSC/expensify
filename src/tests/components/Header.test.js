b10a504eec5d56cbe403656cdfde38bf2a997411
KamajiiSC <brandonlschaen@gmail.com>
2020-06-21 19:34:23 -0400

    Created enzyme wrapper

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
+  const wrapper = shallow(<Header />);
   // const renderer = new ReactShallowRenderer();
   // renderer.render(<Header />)
   // expect(renderer.getRenderOutput()).toMatchSnapshot();
