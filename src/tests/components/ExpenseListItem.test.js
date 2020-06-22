1a3867fe1465c1084b4b23f41c5a3c5465fccecf
KamajiiSC <brandonlschaen@gmail.com>
2020-06-22 16:43:38 -0400

    Created test for rendering a list item

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -2,3 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseListItem } from '../../components/ExpenseListItem';
 import expenses from '../fixtures/expenses';
+
+test('should render an ExpenseListItem with data from fixture', () => {
+  
+});
