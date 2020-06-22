6ed53a1aa06e69b9ce30bac32a0dd36b544594c0
KamajiiSC <brandonlschaen@gmail.com>
2020-06-22 15:24:20 -0400

    Added conditional logic to show a paragraph if the list is empty

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,12 +5,19 @@ import selectExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => (
   <div>
-    {props.expenses.map((expense) => {
-        return <ExpenseListItem 
+    {
+      props.expenses.length === 0 ? (
+        <p>No expenses</p>
+      ) : (
+        props.expenses.map((expense) => {
+        return 
+        <ExpenseListItem 
         key = {expense.id}
         {...expense}
         />;
-      })}
+      })
+      )
+    }
   </div>
 );
 
