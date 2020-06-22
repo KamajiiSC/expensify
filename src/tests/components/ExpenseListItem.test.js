17260c58b9e443503a83a1d6bbd51b8ea64e71cf
KamajiiSC <brandonlschaen@gmail.com>
2020-06-22 17:01:02 -0400

    Fixed return statement in ExpenseList and added an expectation for wrappe in ExpenseListItem test

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -5,4 +5,5 @@ import expenses from '../fixtures/expenses';
 
 test('should render an ExpenseListItem with data from fixture', () => {
   const wrapper = shallow(<ExpenseListItem expense={expenses[1]} />)
+  expect(wrapper).toMatchSnapshot()
 });
