7840b51a626a7ad8eec7c29298fb7ad5e3e18be4
KamajiiSC <brandonlschaen@gmail.com>
2020-06-22 17:29:44 -0400

    Set expectation and created snapshot for test

--- src/tests/components/ExpenseDashboardPage.test.js
+++ src/tests/components/ExpenseDashboardPage.test.js
@@ -4,4 +4,5 @@ import ExpenseDashboardPage from '../../components/ExpenseDashboardPage';
 
 test('should render a default ExpenseDashboardPage', () => {
   const wrapper = shallow(<ExpenseDashboardPage />);
+  expect(wrapper).toMatchSnapshot();
 });
