c2916ae871fff70ee992c2bd4d7c8097aac8cfa9
KamajiiSC <brandonlschaen@gmail.com>
2020-06-22 17:12:55 -0400

    Fixed imports so ExpenseListItem is imported as default but shallow is named imported

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { ExpenseListItem } from '../../components/ExpenseListItem';
+import {shallow} from 'enzyme';
+import ExpenseListItem  from '../../components/ExpenseListItem';
 import expenses from '../fixtures/expenses';
 
 test('should render an ExpenseListItem with data from fixture', () => {
-  const wrapper = shallow(<ExpenseListItem expense={expenses[1]} />)
-  expect(wrapper).toMatchSnapshot()
+  const wrapper = shallow(<ExpenseListItem  expense={expenses[1]}/>)
+  expect(wrapper).toMatchSnapshot();
 });
