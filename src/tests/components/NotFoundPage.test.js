de32478c9326d0ff8d47801a326865e0847e267b
KamajiiSC <brandonlschaen@gmail.com>
2020-06-22 17:31:30 -0400

    Created test for not found page

--- src/tests/components/NotFoundPage.test.js
+++ src/tests/components/NotFoundPage.test.js
@@ -2,3 +2,6 @@ import React from 'react';
 import NotFoundPage from '../../components/NotFoundPage';
 import { shallow } from 'enzyme';
 
+test('should render an instance of the not found page', () => {
+
+});
