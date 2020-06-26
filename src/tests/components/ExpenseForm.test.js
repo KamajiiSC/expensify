bdbe7e742b9efe0fc4c15d49dcf5ddbe45250174
KamajiiSC <brandonlschaen@gmail.com>
2020-06-26 17:06:47 -0400

    Passed prevent default into form

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -15,5 +15,7 @@ test('should render Expense Form with data provided', () => {
 
 test('should render error for invalid form submission', () => {
   const wrapper = shallow(<ExpenseForm />);
-  wrapper.find('form').simulate('submit');
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => {}
+  });
 });
