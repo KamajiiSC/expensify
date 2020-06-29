01901239de0f983605d43270a758e3df6afb5828
KamajiiSC <brandonlschaen@gmail.com>
2020-06-29 15:37:13 -0400

    Defined wrapper and passed an expense and the onSubmitSpy

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -61,6 +61,5 @@ test('should not set amount if input is invalid', () => {
 
 test('should call onSubmit prop for valid form submission', () => {
   const onSubmitSpy = jest.fn();
-  onSubmitSpy();
-  expect(onSubmitSpy).toHaveBeenCalled();
+  const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy}/>)
 });
