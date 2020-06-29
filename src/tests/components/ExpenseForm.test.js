7916784df9c86b3b8483859363a2f2831f032cb6
KamajiiSC <brandonlschaen@gmail.com>
2020-06-29 15:38:36 -0400

    Simulated the form for submit

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -62,4 +62,7 @@ test('should not set amount if input is invalid', () => {
 test('should call onSubmit prop for valid form submission', () => {
   const onSubmitSpy = jest.fn();
   const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy}/>)
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => {}
+  });
 });
