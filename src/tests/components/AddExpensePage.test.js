c2c29f80ddb2a87959f5c47b2bcbdb27df26c068
KamajiiSC <brandonlschaen@gmail.com>
2020-07-03 16:22:25 -0400

    Defined onSubmit with jest

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -1,3 +1,7 @@
 import React from 'react';
 import {shallow } from 'enzyme';
 import { AddExpensePage} from '../../components/AddExpensePage';
+
+test('should render AddExpensePage correctly', () => {
+  const onSubmit = jest.fn();
+});
