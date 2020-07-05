ccaaa1a824a2d3c51aa0720b09a2eb2ae0c48509
KamajiiSC <brandonlschaen@gmail.com>
2020-07-05 15:39:36 -0400

    Removed .dispatch in class and fixed rmeoveExpense call

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -6,11 +6,11 @@ import { editExpense } from '../actions/expenses'
 
 export class EditExpensePage extends React.Component {
   onSubmit = (expense) => {
-    this.props.dispatch(editExpense(this.props.expense.id, expense));
+    this.props.editExpense(this.props.expense.id, expense);
     this.props.history.push('/');
   };
   onRemove = () => {
-    this.props.dispatch(removeExpense({ id: this.props.expense.id }));
+    this.props.removeExpense({ id: this.props.expense.id });
     this.props.history.push('/');
   }
   render() {
