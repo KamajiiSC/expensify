466171527ec2098336cfc12c9a57fcf114ae4bed
KamajiiSC <brandonlschaen@gmail.com>
2020-07-06 15:29:42 -0400

    Created a beforeEach to use the variables fresh each time

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -4,3 +4,11 @@ import { ExpenseListFilters } from '../../components/ExpenseListFilters';
 import { filters, altFilters } from '../fixtures/filters';
 
 let setTextFilter, sortByDate, sortByAmount,setStartDate, setEndDate;
+
+beforeEach(() => {
+  setTextFilter = jest.fn();
+  sortByDate = jest.fn();
+  sortByAmount = jest.fn();
+  setStartDate = jest.fn();
+  setEndDate = jest.fn();
+});
