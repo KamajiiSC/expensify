bb32ce85bfac020177f3eb3a6f0c27e509dbe9c5
KamajiiSC <brandonlschaen@gmail.com>
2020-07-06 15:21:02 -0400

    Defined altFilters to test using real data

--- src/tests/fixtures/filters.js
+++ src/tests/fixtures/filters.js
@@ -6,3 +6,10 @@ const filters = {
   startDate: undefined,
   endDate: undefined
 };
+
+const altFilters = {
+  text: 'bills',
+  sortBy: 'amount',
+  startDate: moment(0),
+  endDate: moment(0).add(3, 'days')
+};
