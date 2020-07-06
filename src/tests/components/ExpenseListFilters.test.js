0c8cd77fd89846c582b0c7eec1bedbf4eae9cbc7
KamajiiSC <brandonlschaen@gmail.com>
2020-07-06 15:48:04 -0400

    Cleanup syntax

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';;
+import { shallow } from 'enzyme';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
 import { filters, altFilters } from '../fixtures/filters';
 
