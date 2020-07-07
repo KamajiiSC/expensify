5c0ac5bcdb0452bf16449790e25bcdfdc0361a79
KamajiiSC <brandonlschaen@gmail.com>
2020-07-06 20:02:34 -0400

    Set onFocusChange prop to calendarFocused and expected the state of calendarFocused to be the new value

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -69,4 +69,6 @@ test('should handle date changes', () => {
 // should handle date focus changes
 test('should handle date focus changes', () => {
   const calendarFocused = 'endDate';
+  wrapper.find(DateRangePicker).prop('onFocusChange')(calendarFocused);
+  expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
 });
